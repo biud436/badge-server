6813652bef1141bca8ea21dbbfb78cda6e5069b5
biud436 <biud436@gmail.com>
2021-01-08 18:26:40 +0900

    init

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const http = require('http');
+const fs = require('fs');
+
+const port = 3000;
+const url = 'https://biud436.com:9001';
+
+function status(req, res) {
+       http.get(url, function (response) {
+            // Its Online
+            res.writeHead(200, {
+                'Content-Type': 'image/svg+xml'
+            });
+            fs.createReadStream('../badges/online.svg').pipe(res);
+        }).on('error', function(e) {
+            // Its Offline
+            res.writeHead(200, {
+                'Content-Type': 'image/svg+xml'
+            });
+            fs.createReadStream('../badges/offline.svg').pipe(res);            
+    });
+};
+
+http.createServer(status).listen(port);
