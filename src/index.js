0d154f015a6e452c9df40a5a6e53f76bfc011048
biud436 <biud436@gmail.com>
2021-01-08 18:50:44 +0900

    .

--- src/index.js
+++ src/index.js
@@ -1,23 +1,71 @@
-const http = require('http');
+// const http = require('http');
+// const fs = require('fs');
+
+// const port = 9003;
+// const url = 'https://biud436.com:9001';
+
+// function status(req, res) {
+//        http.get(url, function (response) {
+//             // Its Online
+//             res.writeHead(200, {
+//                 'Content-Type': 'image/svg+xml'
+//             });
+//             fs.createReadStream('../badges/online.svg').pipe(res);
+//         }).on('error', function(e) {
+//             // Its Offline
+//             res.writeHead(200, {
+//                 'Content-Type': 'image/svg+xml'
+//             });
+//             fs.createReadStream('../badges/offline.svg').pipe(res);            
+//     });
+// };
+
+// http.createServer(status).listen(port);
+
+const express = require("express")
+const http = require("http");
+const https = require("https");
+const static = require("serve-static");
+const bodyParser = require("body-parser");
+const path = require('path');
+const cors = require("cors");
 const fs = require('fs');
 
-const port = 9003;
-const url = 'https://biud436.com:9001';
+// 익스프레스 객체 생성
+const app = express();
 
-function status(req, res) {
-       http.get(url, function (response) {
-            // Its Online
-            res.writeHead(200, {
-                'Content-Type': 'image/svg+xml'
-            });
-            fs.createReadStream('../badges/online.svg').pipe(res);
-        }).on('error', function(e) {
-            // Its Offline
-            res.writeHead(200, {
-                'Content-Type': 'image/svg+xml'
-            });
-            fs.createReadStream('../badges/offline.svg').pipe(res);            
+app.set("port", process.env.PORT || 9003);
+app.use(static(path.resolve("public")));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(bodyParser.json());
+app.use(express.json());
+app.use(cors());
+
+const router = express.Router();
+
+router.route('*.svg').get((req, res) => {
+    https.get("https://biud436.com:9001", function (response) {
+        // Its Online
+        res.writeHead(200, {
+            'Content-Type': 'image/svg+xml'
+        });
+        fs.createReadStream(path.resolve(__dirname, '../public/badges/online.svg')).pipe(res);
+    }).on('error', function (e) {
+        // Its Offline
+        res.writeHead(200, {
+            'Content-Type': 'image/svg+xml'
+        });
+        fs.createReadStream(path.resolve(__dirname, '../public/badges/offline.svg')).pipe(res);
     });
-};
+});
 
-http.createServer(status).listen(port);
+// 라우터를 미들웨어로 등록
+app.use("/", router);
+
+// 서버 생성
+const server = http.createServer(app).listen(app.get("port"), () => {
+
+    console.log(`server start : %d`, app.get("port"));
+});
