cb647390511682c15a401e33bfc621b9fa86a8b4
biud436 <biud436@gmail.com>
2021-01-13 20:07:38 +0900

    .

--- src/index.js
+++ src/index.js
@@ -1,28 +1,6 @@
-// const http = require('http');
-// const fs = require('fs');
+const express = require("express");
+require('express-async-errors');
 
-// const port = 9003;
-// const url = 'https://biud436.com:9001';
-
-// function status(req, res) {
-//        http.get(url, function (response) {
-//             // Its Online
-//             res.writeHead(200, {
-//                 'Content-Type': 'image/svg+xml'
-//             });
-//             fs.createReadStream('../badges/online.svg').pipe(res);
-//         }).on('error', function(e) {
-//             // Its Offline
-//             res.writeHead(200, {
-//                 'Content-Type': 'image/svg+xml'
-//             });
-//             fs.createReadStream('../badges/offline.svg').pipe(res);            
-//     });
-// };
-
-// http.createServer(status).listen(port);
-
-const express = require("express")
 const http = require("http");
 const https = require("https");
 const static = require("serve-static");
@@ -30,6 +8,7 @@ const bodyParser = require("body-parser");
 const path = require('path');
 const cors = require("cors");
 const fs = require('fs');
+const ShortUrl = require('./mysql');
 
 // 익스프레스 객체 생성
 const app = express();
@@ -45,6 +24,7 @@ app.use(cors());
 
 const router = express.Router();
 
+// Controller
 router.route('/project_one').get((req, res) => {
     https.get("https://biud436.com:9001", function (response) {
         // Its Online
@@ -63,11 +43,70 @@ router.route('/project_one').get((req, res) => {
     });
 });
 
+// Controller
+router.route("/insertUrl").get((req, res) => {
+    const query = req.query;
+    const shortUrl = new ShortUrl();
+    const url = query.url;
+    shortUrl.insert(url);
+
+    shortUrl.get(key, (err, result) => {
+        if(err) {
+            res.json({
+                success: false,
+                message: err
+            });
+            
+            return;
+        }
+        const url = result[0].urlValue;
+
+        res.json({
+            success: true,
+            shortUrl: url
+        });        
+    });
+});
+
+router.route("/*").get(async (req, res, next) => {
+    let key = req.path.slice(1);
+
+    const shortUrl = new ShortUrl();
+    await shortUrl.get(key, (err, result) => {
+
+        myErr = () => {
+            res.writeHead('200', {
+                "Content-Type": "text/html; charset=utf-8"
+            });
+            res.write(JSON.stringify({
+                success: false,
+                message: err
+            }));
+            res.end();
+        };
+
+        if (err) {
+            myErr();
+            next();
+            return;
+        }
+
+        const url = result[0].urlValue;
+
+        if (url) {
+            res.redirect(url);
+        } else {
+            myErr();
+        }
+
+    });    
+
+});
+
 // 라우터를 미들웨어로 등록
 app.use("/", router);
 
 // 서버 생성
 const server = http.createServer(app).listen(app.get("port"), () => {
-
     console.log(`server start : %d`, app.get("port"));
 });
---
+++ src/mysql.js
@@ -0,0 +1,149 @@
+const mysql = require("mysql2");
+const path = require("path");
+const fs = require("fs");
+const settings = require("../config.json");
+
+class SQLManagerImpl {
+
+    constructor() {
+    }
+
+    createConnection() {
+        return new Promise((resolve, reject) => {
+            const pool = mysql.createPool({
+                connectionLimit: 10,
+                host: settings.host,
+                port: settings.port,
+                user: settings.user,
+                password: settings.password,
+                database: settings.database,
+                debug: false
+            });
+
+            pool.getConnection((err, conn) => {
+                if(err) {
+                    
+                    if(conn) {
+                        conn.release();
+                    }
+
+                    reject(err);
+                    return;
+                }
+                resolve(conn);
+            });
+        });
+    }
+
+    async getConnection() {
+        this._conn = await this.createConnection();
+        return this._conn;
+    }
+
+    async dropTable() {
+        this._conn = await this.createConnection();
+        this._conn.query(`DROP TABLE if EXISTS tblShortUrl`, (err, result) => {
+            if(err) {
+                console.warn(err);
+                conn.release();
+                return;
+            }
+            console.log("drop table 성공");
+        })    
+
+        return this._conn;
+    }
+
+    async createTable(conn) {
+        
+        conn.query(`create table tblShortUrl( urlKey varchar(200) not NULL  PRIMARY KEY NOT NULL, urlValue varchar(255) NOT NULL )`, (err, result) => {
+            if(err) {
+                console.warn(err);
+                conn.release();
+                return;
+            }
+            console.log("테이블 생성 완료");
+        });
+
+        return conn;
+    }
+
+    async insertData(conn, myUrl) {
+        try {
+
+            const url = decodeURIComponent( myUrl );
+
+            const query = `INSERT INTO tblShortUrl(urlKey, urlValue) VALUES( LEFT(MD5(?), 6), ? )`;
+            const exec = conn.query(query, [
+                url,
+                url
+            ], (err, result) => {
+                conn.release();
+            });
+
+        } catch(e) {
+            console.warn(e);
+        }
+
+        return conn;
+    }
+
+    async getData(conn, key) {
+        return new Promise((resolve, reject) => {
+            try {
+                const query = `SELECT urlValue FROM tblShortUrl WHERE urlKey = ?`;
+                const exec = conn.query(query, [
+                    key
+                ], (err, result) => {
+                    conn.release();
+
+                    if(err) {
+                        reject(err);
+                        return;
+                    }
+
+                    if(result.length > 0) {
+                        resolve(result);
+                    } else {
+                        reject("데이터를 찾을 수 없습니다.");
+                    }
+
+                });
+    
+            } catch(e) {
+                console.warn(e);
+            }
+        })
+
+    }
+}
+
+class ShortUrl {
+    async insert(url) {
+        try {
+            const man = new SQLManagerImpl();
+    
+            man.getConnection().then(async (conn) => {
+                await man.insertData(conn, url);
+            }).catch(err => console.warn(err));
+            
+        } catch(e) {
+            console.warn(e);
+        }        
+    }    
+
+    async get(key, callback) {
+        const man = new SQLManagerImpl();
+        man.getConnection().then(async (conn) => {
+            man.getData(conn, key).then(result => {
+                callback(false, result)
+            }).catch(err => {
+                callback(true, result);
+            });
+        }).catch(err => {
+            console.warn(err);
+        });
+    }
+}
+
+module.exports = ShortUrl;
